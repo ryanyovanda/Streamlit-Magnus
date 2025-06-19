5e82035d9cca8525191c0487e2152cda71fc73ed
Ryan Yovanda <ryanyovanda@gmail.com>
2025-06-19 14:52:00 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="EEG Muse Dashboard", layout="wide")
+st.title("🧠 Welcome to EEG Muse 2 Dashboard")
+st.markdown("Silakan pilih halaman dari sidebar.")
